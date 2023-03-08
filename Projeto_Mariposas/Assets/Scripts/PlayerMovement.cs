9a3d0de54a7662e5ea89da7b8cd6f8badd7487d4
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2023-03-08 16:03:00 -0300

    Add horizontal movement + Animation

---
+++ Projeto_Mariposas/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+    }
+}
