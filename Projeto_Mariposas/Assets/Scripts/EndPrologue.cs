47071df525ae4862658ac069b5588da772001869
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2023-06-15 04:57:35 -0300

    Prologue update

---
+++ Projeto_Mariposas/Assets/Scripts/EndPrologue.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EndPrologue : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector director;
+
+    public void PlayCutscene(){
+        director.Play();
+    }
+}
