33af5dcd89d0e5614a531633fa72804529421fbe
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2024-02-18 19:47:51 -0300

    GameManager

---
+++ Projeto_Mariposas/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    public GameState State;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+    
+    void Start()
+    {
+        ChangeGameState(GameState.menu);
+    }
+
+    public void ChangeGameState(GameState newState) {
+        State = newState;
+
+        switch(newState){
+            case GameState.menu:
+                break;
+            case GameState.cutscene:
+                break;
+            case GameState.gameplay:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+
+    }
+}
+
+public enum GameState {
+    menu,
+    cutscene,
+    gameplay
+
+}
