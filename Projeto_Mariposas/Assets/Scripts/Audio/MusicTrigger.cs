549fc91aaa3cb2b640c74ad1d93b46060d9052de
MoaiDeChapeu <lucaspezenti.s@gmail.com>
2024-06-03 12:28:24 -0300

    Intro update

--- Projeto_Mariposas/Assets/Scripts/Audio/MusicTrigger.cs
+++ Projeto_Mariposas/Assets/Scripts/Audio/MusicTrigger.cs
@@ -10,7 +10,7 @@ public class MusicTrigger : MonoBehaviour
     {
         audioManager = AudioManager.GetInstance();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
